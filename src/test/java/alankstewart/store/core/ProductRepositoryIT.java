3a5e2172af7cad723ebde4e0aaccdad349645f67
alanstewart <alankstewart@gmail.com>
2015-12-26 16:38:08 +1100

    Added new test

--- src/test/java/alankstewart/store/core/ProductRepositoryIT.java
+++ src/test/java/alankstewart/store/core/ProductRepositoryIT.java
@@ -7,7 +7,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -28,5 +32,18 @@ public class ProductRepositoryIT extends AbstractIntegrationTest {
         assertThat(products, is(notNullValue()));
         assertThat(products.getTotalPages(), is(1));
         assertThat(products.getTotalElements(), is(2L));
+    }
+
+    @Test
+    public void shouldFindProductByAttribute() {
+        Product product = new Product("iPhone", new BigDecimal("1000"), "iPhone 6S Plus 64Gb");
+        product.setAttribute("case", "black leather");
+        product = productRepository.save(product);
+        assertThat(product, is(notNullValue()));
+        System.out.println(product);
+        List<Product> products = productRepository.findByAttributeAndValue("case", "black leather");
+        assertThat(products, hasSize(1));
+        product = products.get(0);
+        assertThat(product.getName(), is("iPhone"));
     }
 }
